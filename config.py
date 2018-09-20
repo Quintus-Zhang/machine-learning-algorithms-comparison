80f027eb928a065b35d84670ad1b1a14071871a9
Quintus <quancheng.zhang@rutgers.edu>
2018-09-20 13:50:19 -0400

    - update analysis framework for dtree

--- config.py
+++ config.py
@@ -6,10 +6,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # sklearn
-from sklearn.model_selection import train_test_split, learning_curve, ShuffleSplit
+from sklearn.model_selection import train_test_split, learning_curve, ShuffleSplit, validation_curve, GridSearchCV, \
+    cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve, auc, precision_recall_curve, \
-    average_precision_score, f1_score, log_loss, classification_report, confusion_matrix
+    average_precision_score, f1_score, log_loss, classification_report, confusion_matrix, make_scorer
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -17,7 +18,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 # local items
 sys.path.insert(0, os.path.dirname(os.getcwd()))
-from utilities import plot_learning_curve
+from utilities import plot_learning_curve, plot_validation_curve
 
 
 # Set up directory and file path
--- utilities.py
+++ utilities.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import learning_curve
+from sklearn.model_selection import learning_curve, validation_curve
+from sklearn.metrics import make_scorer
 
 
-def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
-                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, metric=None,
+                        n_jobs=4, train_sizes=np.linspace(.1, 1.0, 5)):
     """
     Generate a simple plot of the test and training learning curve.
 
@@ -50,14 +51,22 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
     if ylim is not None:
         plt.ylim(*ylim)
     plt.xlabel("Training examples")
-    plt.ylabel("Score")
+
+    if metric is None:
+        scorer = None
+        plt.ylabel("Accuracy")
+    else:
+        scorer = make_scorer(metric)
+        plt.ylabel(metric.__name__)
+
     train_sizes, train_scores, test_scores = learning_curve(
-        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+        estimator, X, y, cv=cv, scoring=scorer, n_jobs=n_jobs, train_sizes=train_sizes)
+
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
     test_scores_std = np.std(test_scores, axis=1)
-    plt.grid()
+    plt.grid(linewidth=1)
 
     plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
                      train_scores_mean + train_scores_std, alpha=0.1,
@@ -72,3 +81,34 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
     plt.legend(loc="best")
     return plt
 
+
+def plot_validation_curve(estimator, title, X, y, ylim=None, cv=None, metric=None,
+                          n_jobs=4, param_name=None, param_range=None):
+
+    scorer = make_scorer(metric)
+    train_scores, test_scores = validation_curve(
+        estimator, X, y, param_name=param_name, param_range=param_range,
+        cv=cv, scoring=scorer, n_jobs=n_jobs)
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    plt.title(title)
+    plt.xlabel(param_name)
+    plt.ylabel(metric.__name__)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    lw = 2
+    plt.plot(param_range, train_scores_mean, label="Training score", color="darkorange", lw=lw)
+    plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.2,
+                     color="darkorange", lw=lw)
+    plt.plot(param_range, test_scores_mean, label="Cross-validation score", color="navy", lw=lw)
+    plt.fill_between(param_range, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.2,
+                     color="navy", lw=lw)
+    plt.legend(loc="best")
+    print(param_range[test_scores_mean.argmax()])
+    return plt
